9571120ea2cc8ea3b05fe42239bfafe7ccad0a1a
Arthur de Carvalho Miranda <104659615+arthurmiranda01@users.noreply.github.com>
2023-02-07 11:32:19 -0300

    finalizações

--- urna/script2.js
+++ urna/script2.js
@@ -1,27 +1,64 @@
 let seuVotoPara = document.querySelector('.d-1-1 span');
 let cargo = document.querySelector('.d-1-2 span');
 let descricao = document.querySelector('.d-1-4');
-let aviso  = document.querySelector('.d-2');
+let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
+let numero = '';
 
 function comecarEtapa() {
 
     let etapa = etapas[etapaAtual];
     let numeroHTML = '';
+
+    for(let i=0; i<etapa.numeros; i++) {
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else{
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
     seuVotoPara.style.display = 'none';
     cargo.innerHTML = etapa.titulo;
     descricao.innerHTML = '';
     aviso.style.display = 'none';
     lateral.innerHTML = '';
-    numeros.innerHTML = numeroHTML;
+    numeros.innerHTML = numeroHtml;
 
 }
 
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.numero === numero) {
+            return true
+        } else {
+            return false
+        }
+    });
+    if(candidato.length > 0){
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/> Partido: ${candidato.partido}`;
+    }
+}
+
 function clicou(n){
-    alert('Clicou em ' + n)
+    let elNumero = document.querySelector('.numero.pisca');
+        if(elNumero !== null) {
+            elNumero.innerHTML = n;
+            numero = `${numero}${n}`;
+            elNumero.classList.remove('pisca');
+            if(elNumero.nextElementSibling !== null) {
+                elNumero.nextElementSibling.classList.add('pisca');//nextElementSubling é para mandar uma ação para o próximo elemento
+            }else{
+                atualizaInterface();
+            }
+        }
 }
 
 function branco(){
@@ -34,4 +71,8 @@ function corrige(){
 
 function confirma(){
     alert('Clicou em CONFIRMAR')
+}
+
+function comecarEtapa(){
+
 }
