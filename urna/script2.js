079c5c65803f3b0c2d5300dda798d0c884db4c21
Arthur de Carvalho Miranda <104659615+arthurmiranda01@users.noreply.github.com>
2023-02-06 21:58:46 -0300

    inicio do script

---
+++ urna/script2.js
@@ -0,0 +1,37 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso  = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+
+function comecarEtapa() {
+
+    let etapa = etapas[etapaAtual];
+    let numeroHTML = '';
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHTML;
+
+}
+
+function clicou(n){
+    alert('Clicou em ' + n)
+}
+
+function branco(){
+    alert('Clicou em BRANCO')
+}
+
+function corrige(){
+    alert('Clicou em CORRIGIR')
+}
+
+function confirma(){
+    alert('Clicou em CONFIRMAR')
+}
